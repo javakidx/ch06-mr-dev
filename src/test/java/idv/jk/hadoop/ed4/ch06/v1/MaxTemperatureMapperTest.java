e2c5dd1bd56e4241d3c0170c73c0ddbf8c91b376
javakidx <javakidx@gmail.com>
2015-06-25 13:15:32 +0800

    mapreduce.MapDriver

--- src/test/java/idv/jk/hadoop/ed4/ch06/v1/MaxTemperatureMapperTest.java
+++ src/test/java/idv/jk/hadoop/ed4/ch06/v1/MaxTemperatureMapperTest.java
@@ -1,11 +1,11 @@
 package idv.jk.hadoop.ed4.ch06.v1;
 
-import idv.jk.hadoop.ed3.ch02.oldapi.*;
-import idv.jk.hadoop.ed3.ch02.oldapi.MaxTemperatureMapper;
+//import idv.jk.hadoop.ed3.ch02.oldapi.*;
+//import idv.jk.hadoop.ed3.ch02.oldapi.MaxTemperatureMapper;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mrunit.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -19,13 +19,27 @@ public class MaxTemperatureMapperTest
     public void processValidRecord() throws IOException, InterruptedException
     {
         Text value = new Text("0043011990999991950051518004+68750+023550FM-12+0382" +
-                // Year ^^^^
+                                      // Year ^^^^
                 "99999V0203201N00261220001CN9999999N9-00111+99999999999");
-        // Temperature ^^^^^
+                                       // Temperature ^^^^^
         new MapDriver<LongWritable, Text, Text, IntWritable>()
                 .withMapper(new MaxTemperatureMapper())
                 .withInput(new LongWritable(0), value)
                 .withOutput(new Text("1950"), new IntWritable(-11))
+                .runTest();
+    }
+
+    @Test
+    public void ignoresMissingTemperatureRecord() throws IOException, InterruptedException
+    {
+        Text value = new Text("0043011990999991950051518004+68750+023550FM-12+0382" +
+                                      // Year ^^^^
+                "99999V0203201N00261220001CN9999999N9+99991+99999999999");
+                                       // Temperature ^^^^^
+
+        new MapDriver<LongWritable, Text, Text, IntWritable>()
+                .withMapper(new MaxTemperatureMapper())
+                .withInput(new LongWritable(0), value)
                 .runTest();
     }
 }
